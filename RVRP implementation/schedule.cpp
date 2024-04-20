7af40a6068ab3a8bf21e7c5d7f4cc2014eac0c72
zsolt sex <kolumbanzsolt2004@gmail.com>
2024-04-20 17:51:30 +0300

    final

--- RVRP implementation/schedule.cpp
+++ RVRP implementation/schedule.cpp
@@ -1,68 +1,64 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <stack>
-#include <unordered_map>
-#include <map>
-#include <fstream>
-#include <queue>
 #include <set>
-
 using namespace std;
-typedef struct Node{
-    int id;
-    int weight;
-    int ctime;
-    set<int> currStack;
-}Node;
 
+// Global variables (as per your provided code)
 int nr_of_clients = 5;
 int time_g = 9;
 int dist[5][5] = {
-    {0,3,2,1,4},
-    {3,0,3,1,5},
-    {2,3,0,3,1},
-    {1,1,3,0,5},
-    {4,5,1,5,0}
-};
-int weight[10] = {0,1,3,5,3};
+    {0, 3, 2, 1, 4},
+    {3, 0, 3, 1, 5},
+    {2, 3, 0, 3, 1},
+    {1, 1, 3, 0, 12},
+    {4, 5, 1, 12, 0}};
+int weight[5] = {0, 1, 3, 5, 3}; // Adjusted the size to match the number of clients
+// Function to find the maximum weight path
+void findMaxWeightPath(int node, vector<int>& path, int currentWeight, int currentTime, vector<int>& bestPath, int& maxWeight, set<int>& visited) {
+    // Mark the current node as visited
+    visited.insert(node);
 
-void bfs(){
-    Node graph[5];
-    for(int i=0; i<nr_of_clients; i++){
-        graph[i] = {i,0,0,{i}};
+    // Add the current node to the path
+    path.push_back(node);
+    currentWeight += weight[node];
+
+    // If the current path is better, update the best path and max weight
+    if (currentWeight > maxWeight && currentTime <= time_g) {
+        maxWeight = currentWeight;
+        bestPath = path;
     }
-    queue<Node> q;
-    q.push(graph[0]);
-    while(!q.empty()){
-        Node curr = q.front();
-        q.pop();
-        for(int i=0; i<nr_of_clients; i++){
-            //cout<<curr.id<<" "<<graph[i].id<<" "<<dist[graph[i].id][curr.id]<<"+"
-            //<<curr.ctime<<endl;
-            if(curr.currStack.find(graph[i].id) == curr.currStack.end()
-            && curr.weight + weight[graph[i].id] > graph[i].weight
-            && dist[graph[i].id][curr.id] + curr.ctime <= time_g){
-                graph[i].currStack = curr.currStack;
-                graph[i].currStack.insert(graph[i].id);
-                graph[i].ctime = curr.ctime + dist[graph[i].id][curr.id];
-                graph[i].weight = curr.weight + weight[graph[i].id];
-                q.push(graph[i]);
+
+    // Explore all adjacent nodes
+    for (int nextNode = 0; nextNode < nr_of_clients; ++nextNode) {
+        if (visited.find(nextNode) == visited.end()) { // Check if the node is not already visited
+            int nextTime = currentTime + dist[node][nextNode];
+            if (nextTime <= time_g) {
+                findMaxWeightPath(nextNode, path, currentWeight, nextTime, bestPath, maxWeight, visited);
             }
         }
     }
-    for(int i=0; i<nr_of_clients; i++){
-        cout<<graph[i].id<<": time="<<graph[i].ctime<<",  weight="<<graph[i].weight<<endl;
-        cout<<"Stack: {";
-        for(auto k:graph[i].currStack){
-            cout<<k<<", ";
-        }
-        cout<<"}"<<endl<<endl;
-    }
+
+    // Backtrack: remove the current node from the path and mark it as unvisited
+    path.pop_back();
+    visited.erase(node);
 }
 
-int main()
-{
-    bfs();
+int main() {
+    vector<int> path, bestPath;
+    set<int> visited;
+    int maxWeight = 0;
+
+    // Try each node as a starting point
+    for (int startNode = 0; startNode < nr_of_clients; ++startNode) {
+        findMaxWeightPath(startNode, path, 0, 0, bestPath, maxWeight, visited);
+    }
+
+    // Output the best path and its weight
+    cout << "Best path with maximum weight sum within time limit:" << endl;
+    for (int node : bestPath) {
+        cout << node << " ";
+    }
+    cout << "\nMaximum weight sum: " << maxWeight << endl;
+
     return 0;
 }
