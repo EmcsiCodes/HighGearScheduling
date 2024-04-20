7d067311e8b653ac33dc767ac7090d53adbf4818
EmcsiCodes <kasztaemmanuel@gmail.com>
2024-04-20 17:11:18 +0300

    m

--- RVRP implementation/schedule.cpp
+++ RVRP implementation/schedule.cpp
@@ -5,25 +5,64 @@
 #include <unordered_map>
 #include <map>
 #include <fstream>
+#include <queue>
+#include <set>
 
 using namespace std;
+typedef struct Node{
+    int id;
+    int weight;
+    int ctime;
+    set<int> currStack;
+}Node;
 
-int nr_of_clients = 10;
+int nr_of_clients = 5;
+int time_g = 9;
 int dist[5][5] = {
-    {0, 5, 99, 93, 45, 91, 58, 56, 7, 92},
-    {5, 0, 88, 3, 90, 21, 80, 55, 85, 12},
-    {99, 88, 0, 46, 8, 30, 99, 68, 67, 80},
-    {93, 3, 46, 0, 68, 9, 88, 16, 92, 89},
-    {45, 90, 8, 68, 0, 22, 7, 86, 31, 9},
-    {91, 21, 30, 9, 22, 0, 67, 13, 60, 27},
-    {58, 80, 99, 88, 7, 67, 0, 41, 36, 55},
-    {56, 55, 68, 16, 86, 13, 41, 0, 25, 56},
-    {7, 85, 67, 92, 31, 60, 36, 25, 0, 43},
-    {92, 12, 80, 89, 9, 27, 55, 56, 43, 0},
+    {0,3,2,1,4},
+    {3,0,3,1,5},
+    {2,3,0,3,1},
+    {1,1,3,0,5},
+    {4,5,1,5,0}
 };
+int weight[10] = {0,1,3,5,3};
+
+void bfs(){
+    Node graph[5];
+    for(int i=0; i<nr_of_clients; i++){
+        graph[i] = {i,0,0,{i}};
+    }
+    queue<Node> q;
+    q.push(graph[0]);
+    while(!q.empty()){
+        Node curr = q.front();
+        q.pop();
+        for(int i=0; i<nr_of_clients; i++){
+            //cout<<curr.id<<" "<<graph[i].id<<" "<<dist[graph[i].id][curr.id]<<"+"
+            //<<curr.ctime<<endl;
+            if(curr.currStack.find(graph[i].id) == curr.currStack.end()
+            && curr.weight + weight[graph[i].id] > graph[i].weight
+            && dist[graph[i].id][curr.id] + curr.ctime <= time_g){
+                graph[i].currStack = curr.currStack;
+                graph[i].currStack.insert(graph[i].id);
+                graph[i].ctime = curr.ctime + dist[graph[i].id][curr.id];
+                graph[i].weight = curr.weight + weight[graph[i].id];
+                q.push(graph[i]);
+            }
+        }
+    }
+    for(int i=0; i<nr_of_clients; i++){
+        cout<<graph[i].id<<": time="<<graph[i].ctime<<",  weight="<<graph[i].weight<<endl;
+        cout<<"Stack: {";
+        for(auto k:graph[i].currStack){
+            cout<<k<<", ";
+        }
+        cout<<"}"<<endl<<endl;
+    }
+}
 
 int main()
 {
-
+    bfs();
     return 0;
 }
